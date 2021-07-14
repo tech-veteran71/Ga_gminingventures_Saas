3812cd395390662079f271b27d820b7a4c616ec8
tech-veteran71 <tech.veteran71@gmail.com>
2021-07-14 09:25:38 -0400

    added css sheet for into-hompage

--- src/containers/homepage/intro/index.jsx
+++ src/containers/homepage/intro/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-
+import "./index.scss";
 import { DownAngle } from "../../../components/icon";
 
 const Intro = ({ data, stockItem }) => {
