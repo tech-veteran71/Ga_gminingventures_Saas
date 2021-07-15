a1a6723c17d9f7d738d80d66f775e36c8a0044c9
tech-veteran71 <tech.veteran71@gmail.com>
2021-07-15 16:13:10 -0400

    hot fix

--- src/containers/investors/approach/index.jsx
+++ src/containers/investors/approach/index.jsx
@@ -6,7 +6,7 @@ const Approach = ({ data }) => {
   return (
     <div className="bg-secondary py-20 global-x-spacing">
       <div className="max-w-6xl mx-auto">
-        <h3 className="text-secondary text-center mb-3">{data.title}</h3>
+        <h3 className="text-secondary text-center mb-3 uppercase">{data.title}</h3>
         <div className="grid lg:grid-cols-2 gap-8 lg:gap-16 py-8 text-text">
           <div>
             <RRenderer data={data.leftContent} />
